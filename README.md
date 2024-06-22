f461c8f848884126b1f1166f5cab4a47a42ede24
Ravi Gupta <ravigupta3282@gmail.com>
2024-06-22 11:51:04 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# JsNewsApp
+# JsNewsApp 📰
+
+Welcome to the JsNewsApp! This is a simple news app built using HTML, CSS, and JavaScript. It allows you to fetch and display news articles from the News API. 🚀
+
+**Here are the key features:**
+
+- 🖥️ **Fetches random news:** Retrieves top headlines from India (you can customize this).
+- 🔍 **Searches for specific news:** Allows users to search for news articles related to a given query.
+- 🖼️ **Displays news articles with images:** Each news article is presented with its title, description, and a corresponding image.
+- 🔗 **Links to the full article:**  Clicking on an article takes you to its full version on the original news website.
+- ✨ **Responsive design:**  The app adapts to different screen sizes, ensuring a smooth user experience on desktops, tablets, and mobile devices.
+
+**Here's a sneak peek:**
+
+![JsNewsApp Demo](s1.jpeg)
+![JsNewsApp Demo](s2.jpeg)
+
+**Want to learn how to build this app?**  Check out my YouTube tutorial:
+
+[**YouTube Tutorial Link**](https://youtu.be/ZGLKTQVyT0U?si=vk6RwVIKQfXTn1a8)
+
+**Try out the live demo:**
+
+[**GitHub Pages Demo Link**](https://gupta-ravi.github.io/JsNewsApp)
+
+**Let's get started!**
+
+1. **Clone the repository:**
+```bash
+git clone https://github.com/Gupta-Ravi/JsNewsApp
+```
+
+2. Open index.html in your browser.
+
+**To use the app:**
+
+- **Browse random news:** The app loads a set of random news articles upon initial launch.
+- **Search for news:** Enter a keyword or phrase in the search bar and click "Search" to find relevant articles.
+
+**Customization:**
+
+- **Change the news source:** Update the `country` parameter in `app.js` to fetch news from a different country.
+- **Modify the styling:** Customize the appearance of the app by adjusting the CSS in `style.css`.
+
+**Contributing:**
+
+Feel free to fork the repository and contribute improvements or new features! 🤝
+
+**Let me know if you have any questions!** 😄
---
+++ app.js
@@ -0,0 +1,81 @@
+const apiKey = '5326c2d946794d67b05821f93e51ceeb'
+
+const blogContainer = document.getElementById('blog-container')
+const seachInput = document.getElementById('search-input')
+const seachButton = document.getElementById('search-button')
+
+async function fetchRandomNews() {
+    try {
+        const apiUrl = `https://newsapi.org/v2/top-headlines?country=in&pageSize=10&apiKey=${apiKey}`
+
+        response = await fetch(apiUrl)
+        const data = await response.json()
+        console.log(data)
+        articles = data.articles
+        console.log(articles)
+        return articles
+    } catch {
+        console.error("Error Fetching the News", error)
+        return []
+    }
+}
+
+seachButton.addEventListener('click', async () => {
+    query = seachInput.value.trim()
+    if (query !== '') {
+        try {
+            const articles = await fetchQueryArticles(query)
+            displayBlogs(articles)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+})
+
+async function fetchQueryArticles(query) {
+    try {
+        const apiUrl = `https://newsapi.org/v2/everything?q=${query}&pageSize=10&apiKey=${apiKey}`
+
+        response = await fetch(apiUrl)
+        const data = await response.json()
+        articles = data.articles
+        console.log(articles)
+        return articles
+    } catch {
+        console.error("Error Fetching the News", error)
+        return []
+    }
+}
+
+function displayBlogs(articles) {
+    blogContainer.innerHTML = ''
+    articles.forEach(article => {
+        const blogCard = document.createElement('div')
+        blogCard.id = 'blog-card'
+
+        const img = document.createElement('img')
+        img.src = article.urlToImage
+        img.alt = article.title
+
+        const title = document.createElement('h2')
+        const truncatedTitle = article.title.length > 30 ? article.title.slice(0, 30) + '...' : article.title
+        title.textContent = truncatedTitle
+
+        const desc = document.createElement('p')
+        const truncatedDesc = article.description.length > 100 ? article.description.slice(0, 100) + '...' : article.description
+        desc.textContent = truncatedDesc
+
+        blogCard.appendChild(img)
+        blogCard.appendChild(title)
+        blogCard.appendChild(desc)
+        blogCard.addEventListener('click', () => {
+            window.open(article.url, "_blank")
+        })
+        blogContainer.appendChild(blogCard)
+    })
+}
+
+(async () => {
+    const articles = await fetchRandomNews()
+    displayBlogs(articles)
+})()
